88fb8077c7bcd4caff3d4e86d3b89481c99e531d
zuikipuiki <31111953+zuikipuiki@users.noreply.github.com>
2021-09-21 16:56:18 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+// Step by step pizza making progress
+let pizzaMaking = function () {
+
+
+console.log("Make a well in the center of the flour");
+console.log("Put some water, eggs and salt in it");
+console.log("Knead the Dough");
+console.log("Wrap the dough in the plastic wrap");
+console.log("Let the dough sit for 30mins");
+console.log("Check to see if the douch is elastic");
+console.log("Shape the dough into a circle");
+console.log("Put the tomato sauce on the dough");
+console.log("Put the ingredients on top");
+console.log("Sprinkle some cheese");
+console.log("Put the pizza in the oven");
+console.log("Let pizza bake for 20mins");
+console.log("Your pizza is ready to eat!");
+
+}
+
+// Step by step to make Sushi
+let sushiMaking = function () {
+
+console.log("Prepair the raw fish");
+console.log("Cut the vegetables in length");
+console.log("Boil rice");
+console.log("Let it cool off");
+console.log("Put rice on Nori sheet");
+console.log("Put fish and vegetables op top");
+console.log("Roll Sushi");
+console.log("Your sushi is ready to eat!");
+}
+
+sushiMaking ();
+sushiMaking ();
+sushiMaking ();
+pizzaMaking ();
+pizzaMaking ();
